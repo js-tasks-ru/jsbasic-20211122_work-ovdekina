9ffe1f306fb5287f165672aa255cced5ba8d7300
OlgaDekina <work.ovdekina@gmail.com>
2021-12-05 18:12:24 +0200

    2-module resolved

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,16 @@
 let calculator = {
-  // ваш код
+  a: null,
+  b: null,
+  read(arg1, arg2) {
+    this.a = arg1;
+    this.b = arg2;
+  },
+  sum() {
+    return this.a + this.b;
+  },
+  mul() {
+    return this.a * this.b;
+  }
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
