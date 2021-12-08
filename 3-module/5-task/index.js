bb2fd07b1c134157071cebe00d7daf8a79f351ed
OlgaDekina <work.ovdekina@gmail.com>
2021-12-08 23:48:08 +0200

    3-module resolved

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,7 @@
 function getMinMax(str) {
-  // ваш код...
+  const arr = str.split(' ').map(item => +item).filter((item) => item);
+  let obj = {};
+  obj.min = Math.min(...arr);
+  obj.max = Math.max(...arr);
+  return obj;
 }
