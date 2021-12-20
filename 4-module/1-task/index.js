248c9d74e56f7c70ad53bccfa17c0e1240f49171
OlgaDekina <work.ovdekina@gmail.com>
2021-12-20 17:14:05 +0200

    4-module resolved

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,12 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  const list = document.createElement('UL');
+  const arr = friends.map(item => [item.firstName, item.lastName].join(' '));
+
+  for (let i = 0; i < arr.length; i++) {
+    let li = document.createElement('LI');
+    li.innerText = arr[i]
+    list.appendChild(li);
+  }
+  return list;
 }
+
